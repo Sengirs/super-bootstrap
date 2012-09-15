0fa0663d1f0a9492cce7e3bd72d344ed0be7a465
Etienne Depaulis <etienne.depaulis@gmail.com>
2012-09-15 21:36:24 +0200

    Connect As User for Active Admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,33 @@
+# encoding: utf-8
 ActiveAdmin.register User do
+
+  filter :email
+  
+  index do  
+    column :id
+    
+    column("") { |user| link_to "Connect as", switch_to_admin_user_path(user), method: :post, target: "_blank"}
+    
+    column :email
+
+    column t("last_connection") do |user|
+      if user.last_sign_in_at.nil?
+        "-"
+      else
+        user.last_sign_in_at.strftime("%Y-%m-%d")
+      end
+    end
+
+    default_actions 
+  end
+  
+  member_action :switch_to, method: :post do
+    user = User.find(params[:id])
+    
+    sign_out :user if user_signed_in?
+
+    sign_in :user, user
+    redirect_to root_path
+  end
   
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def after_sign_in_path_for(resource_or_scope)
+    if admin_user_signed_in?
+      admin_users_path
+    elsif user_signed_in?
+      root_path
+    end
+  end
+  
 end
