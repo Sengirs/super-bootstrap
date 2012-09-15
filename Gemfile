50d0348fb554bdc7830112cde55e6e81b8c32c73
Etienne Depaulis <etienne.depaulis@gmail.com>
2012-09-15 19:15:42 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,35 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+
+gem 'pg'
+
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'bootstrap-sass', '~> 2.1.0.0'
+  gem 'coffee-rails', '~> 3.2.1'
+  
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'haml'
+gem 'jquery-rails'
+
+gem 'execjs'
+gem 'therubyracer'
+
+
+# Auth
+gem 'devise'
+
+# Admin
+gem 'activeadmin'
+
+# Forms
+gem 'simple_form'
+gem 'country_select'
+
+# Int
+gem 'rails-i18n'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuperBootstrap::Application.config.secret_token = '4883f5f5fbcc6d606cd51db19891156a96f30656fb4696f35018e5c4a7d4b7706ecdb4ba97392b54be5cbb5a89620b811525cd873dae0732a4afd4aa1c6ee2b2'
